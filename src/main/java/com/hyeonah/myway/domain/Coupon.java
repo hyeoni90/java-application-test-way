51054d2f7913c27d6d328994addb9988c45d5322
hyeoni90 <gusk743@gmail.com>
2020-12-26 22:24:57 +0900

    feat: Add domain Coupon, CouponStatus

---
+++ src/main/java/com/hyeonah/myway/domain/Coupon.java
@@ -0,0 +1,52 @@
+package com.hyeonah.myway.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by hyeonahlee on 2020-12-26.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String code;
+    private CouponStatus status = CouponStatus.DRAFT;
+    private String name;
+    private int limitCount;
+    private LocalDateTime expiredDateTime;
+    private Long memberId;
+
+    public Coupon(final int limit, final String name) {
+        this.limitCount = limit;
+        this.name = name;
+    }
+
+    public Coupon(final int limit) {
+        if(limit < 0) {
+            throw new IllegalArgumentException("limit 은 0보다 커야 합니다.");
+        }
+        this.limitCount = limit;
+    }
+
+    public void active() {
+        this.expiredDateTime = LocalDateTime.now().plusDays(1);
+        this.status = CouponStatus.ACTIVE;
+    }
+
+    public void expire() {
+        this.expiredDateTime = LocalDateTime.now();
+        this.status = CouponStatus.ENDED;
+    }
+}
---
+++ src/main/java/com/hyeonah/myway/domain/CouponStatus.java
@@ -0,0 +1,8 @@
+package com.hyeonah.myway.domain;
+
+/**
+ * Created by hyeonahlee on 2020-12-26.
+ */
+public enum CouponStatus {
+    DRAFT, ACTIVE, ENDED
+}
