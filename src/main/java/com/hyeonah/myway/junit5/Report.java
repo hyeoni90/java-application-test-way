957360f3eac2fafa59d49fdfc9e41aef448437a4
hyeoni90 <gusk743@gmail.com>
2021-01-03 00:46:10 +0900

    feat: Add name in Report

--- src/main/java/com/hyeonah/myway/junit5/Report.java
+++ src/main/java/com/hyeonah/myway/junit5/Report.java
@@ -5,6 +5,7 @@ public class Report {
     private final ReportStatus status = ReportStatus.DRAFT;
 
     private final int limit;
+    private String name;
 
     public Report(final int limit) {
         if(limit < 0) {
@@ -13,11 +14,25 @@ public class Report {
         this.limit = limit;
     }
 
+    public Report(final int limit, final String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
     public ReportStatus getStatus() {
         return this.status;
     }
 
     public int getLimit() {
         return limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+            "status=" + status +
+            ", limit=" + limit +
+            ", name='" + name + '\'' +
+            '}';
     }
 }
